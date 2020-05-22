bcb6237c243fd087b00b9c87b0fbd1a3a9bd4309
Salah Eddine Lachkar <lachkar.se@gmail.com>
2020-05-22 22:00:22 +0000

    Update printing format

--- src/migrator.py
+++ src/migrator.py
@@ -7,21 +7,26 @@ def mysql_to_mongodb(schema_name: str, mysql_config: dict, mongodb_config: dict)
     @param {dictionary} mysql_config - The connection configuration of mysql.
     @param {dictionary} mongodb_config - The connection configuration of mongodb."""
 
-    mysql = MySQLConnector(mysql_config).use(schema_name)
-    mongodb = MongoConnector(mongodb_config).create(schema_name)
+    try:
+        mysql = MySQLConnector(mysql_config).use(schema_name)
+        mongodb = MongoConnector(mongodb_config).create(schema_name)
 
-    for table_name in mysql.get_tables_name():
-        table = mysql.get_table(table_name)
+        print(f"Migration {{MySQL => MongoDB}} of `{schema_name}` started:")
+        for table_name in mysql.get_tables_name():
+            table = mysql.get_table(table_name)
+            if mongodb.insert_many(table_name, table):
+                print(" " * 10 + "✓ " + table_name)
+            else:
+                print(" " * 10 + "🞩 " + table_name)
 
-        mongodb.insert_many(table_name, table)
-        print(f"Migration of table {table_name} is done!")
+        mongodb.apply_foreign_keys(mysql.get_foreign_keys())
+        mongodb.remove_primary_keys(mysql.get_primary_keys())
+        print(f"Migration {{MySQL => MongoDB}} finished!\n")
 
-    mongodb.apply_foreign_keys(mysql.get_foreign_keys())
-    mongodb.remove_primary_keys(mysql.get_primary_keys())
-    print(f"Migration of database {schema_name} is done!")
-
-    del mysql
-    del mongodb
+        del mysql
+        del mongodb
+    except Exception as e:
+        print(f"Error: {str(e)}")
 
 
 def mongodb_to_cassandra(schema_name: str, mongodb_config: dict, cassandra_config: dict) -> None:
@@ -29,20 +34,24 @@ def mongodb_to_cassandra(schema_name: str, mongodb_config: dict, cassandra_confi
     @param {string} schema_name - The schema to migrate from and to.
     @param {dictionary} mongodb_config - The connection configuration of mongodb.
     @param {dictionary} cassandra_config - The connection configuration of cassandra."""
-    
-    mongodb = MongoConnector(mongodb_config).use(schema_name)
-    cassandra = CassandraConnector(cassandra_config).create(schema_name)
 
-    for collection_name in mongodb.get_collection_names():
-        collection = mongodb.get_collection(collection_name)
+    try:
+        mongodb = MongoConnector(mongodb_config).use(schema_name)
+        cassandra = CassandraConnector(cassandra_config).create(schema_name)
 
-        cassandra.insert_many(collection_name, collection)
-        print(f"Migration of table {collection_name} is done!")
+        print(
+            f"Migration {{MongoDB => Cassandra}} of `{schema_name}` started:")
+        for collection_name in mongodb.get_collection_names():
+            collection = mongodb.get_collection(collection_name)
+            cassandra.insert_many(collection_name, collection)
+            print(" " * 10 + "✓ " + collection_name)
 
-    print(f"Migration of database {schema_name} is done!")
+        print(f"Migration {{MongoDB => Cassandra}} finished!\n")
 
-    del mongodb
-    del cassandra
+        del mongodb
+        del cassandra
+    except Exception as e:
+        print(f"Error: {str(e)}")
 
 
 if __name__ == "__main__":
